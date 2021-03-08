48f07c974cad5a2717a3b9ae68a4654d3e713d60
chillaah <chillaah@users.noreply.github.com>
2021-03-08 16:55:37 +1000

    tut1

--- Q1_1.m
+++ Q1_1.m
@@ -1,15 +1,17 @@
-% Write the configuration of the vehicle (1x3 vector) in units of metres and degrees.
+%% Write the configuration of the vehicle (1x3 vector) in units of metres and degrees.
 
-Q = [1.3, 2.1, 40]; %[x, y, theta]
+Q = [1.3, 2.1, 40]; % [x y theta]
 
-% Write a 3x3 homogeneous transformation matrix that describes the pose of this robot 
+
+%% Write a 3x3 homogeneous transformation matrix that describes the pose of this robot 
 % with respect to the world coordinate frame.
 
 TR = [ cos(Q(3)) -sin(Q(3)) Q(1)
        sin(Q(3))  cos(Q(3)) Q(2)
                0          0    1 ];
+           
 
-% Write a 3x3 homogeneous transformation matrix that describes the pose of the sensor 
+%% Write a 3x3 homogeneous transformation matrix that describes the pose of the sensor 
 % with respect to the world coordinate frame.
 
 
@@ -23,6 +25,8 @@ SR = [ cos(Q2(3)) -sin(Q2(3)) Q2(1)
             
 TS = TR * SR; % from world to robot and then from robot to sensor
 
-% Write the position of the navigation target relative to the sensor in polar 
+
+%% Write the position of the navigation target relative to the sensor in polar 
 % coordinate form as a 2x1 vector  (in units of metres and degrees, respectively)
-PP = (sqrt(106)/2, 87.05);
+
+PP = [sqrt(106)/2 87.05]; % double check this and everything else
