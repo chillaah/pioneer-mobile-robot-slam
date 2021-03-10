d096bd92208d2d8e34ddf571146c8d375cab85c0
chillaah <chillaah@users.noreply.github.com>
2021-03-10 17:01:21 +1000

    progress

---
+++ Q1_3.m
@@ -0,0 +1,15 @@
+%% Q1.3
+
+% The maximum steering angle in degrees
+
+% an equation that doesn't involve the back wheel radius 'RB'
+MS = ;
+
+% the radius of the arc of the reference point of the car in metres
+R = 
+
+% coordinate of the ICR in meters as a 2x1 vector
+ICR = 
+
+% rotational speeed of right front wheel with respect to left front wheel in revolutions per minute
+DS = 
