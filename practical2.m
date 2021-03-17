272b049db960d6069b9ef2797aaab919dd9a9bf0
Chiran Walisundara <cwalisundara21@gmail.com>
2021-03-17 13:57:56 +1000

    progress

--- practical2.m
+++ practical2.m
@@ -11,10 +11,10 @@ function [lWv, rWv] = practical2(V, W)
     if desiredLinearVel == 0 && desiredAngularVel ~= 0
         % just right moves
         % so it turns on the spot
-        circleRadius = desiredLinearVel / desiredAngularVel; % length from center to middle of axle (radius)
+        circleRadius = robotAxleLength/2; % length from center to middle of axle (radius)
         
-        linearDistance = 2*pi*circleRadius;
-        timeLinear =  circleRadius * linearDistance / desiredLinearVel; % figure out time
+        angularDistance = 2*pi;
+        timeLinear = angularDistance / desiredAngularVel; % figure out time
 
         leftRadius = circleRadius - robotAxleLength/2; % radius from center to left wheel
         rightRadius = circleRadius + robotAxleLength/2; % radius from center to right wheel
@@ -31,17 +31,24 @@ function [lWv, rWv] = practical2(V, W)
         lWv = degPerSecLeft * pi/180;
         rWv = degPerSecRight * pi/180;
         
+        disp('ok');
+        
     elseif desiredAngularVel == 0 && desiredLinearVel ~= 0
         % moves straight
         % both left and right move with same speed
+        % lWv = desiredLinearVel / 
+        
+    elseif desiredAngularVel == 0 && desiredAngularVel == 0
+        
+        lWv = 0;
+        rWv = 0;
         
     else
         % normal hybrid functioning
-        
         circleRadius = desiredLinearVel / desiredAngularVel; % length from center to middle of axle (radius)
         
         linearDistance = 2*pi*circleRadius;
-        timeLinear = linearDistance / desiredLinearVel - 3; % figure out time
+        timeLinear = linearDistance / desiredLinearVel; % figure out time
 
         leftRadius = circleRadius - (robotAxleLength/2); % radius from center to left wheel
         rightRadius = circleRadius + (robotAxleLength/2); % radius from center to right wheel
@@ -52,7 +59,7 @@ function [lWv, rWv] = practical2(V, W)
 
         % velocity of each wheel in degrees per second
         degPerSecLeft = (numRotLeft * 360) / timeLinear;
-        degPerSecRight = (numRotRight * 360) / timeLinear;% - 50;
+        degPerSecRight = (numRotRight * 360) / timeLinear;
         
         % velocity in radians per second
         lWv = degPerSecLeft * pi/180;
