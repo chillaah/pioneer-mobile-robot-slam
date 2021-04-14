5b7ffedb3b95a694ebfaec3b06a137a38b08e6aa
Chiran Walisundara <cwalisundara21@gmail.com>
2021-04-14 17:54:03 +1000

    progress

--- Prac5/prac5n10454012.m
+++ Prac5/prac5n10454012.m
@@ -2,12 +2,13 @@
 % For example, if your student number is n12345678 and this is a solution for Prac 2, 
 % then you should name this function prac2n12345678
 
-function prac5n1234567(robot)
+function prac5n10454012(robot)
 	% The function needs to start with the following call; 
 	%otherwise, your code will not run during automarking.
 	robot.powerON();
     
-
+    
+    
 
 
 	%The function needs to end with the following calls; 
