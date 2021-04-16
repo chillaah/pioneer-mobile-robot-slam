1b7666fb41ade523fdeb701a4059e50a63d96d78
chillaah <cwalisundara21@gmail.com>
2021-04-16 23:02:40 +1000

    progress

--- Functions/control.m
+++ Functions/control.m
@@ -1,13 +1,4 @@
 function vel = control(q, R, speed, path)
-% Inputs:
-%  q is a 1x3 vector giving the current configuration of the robot in units of metres and radians
-%  R is the pure pursuit following distance
-%  speed is the forward speed of the vehicle
-%  path is an Nx2 matrix containing the path as a set of waypoints, columns are x- and y-coordinates respectively.
-% Return:
-%  vel is a 1x2 vector containing the requested velocity and turn rate of the robot [v, omega]
-
-    % clever stuff
 
     % gain constants
     Kv = 1;
--- Functions/euclidian.m
+++ Functions/euclidian.m
@@ -1,4 +1,5 @@
 function GOAL = euclidian(q, LINE)
+
     a = LINE(1); b = LINE(2); c = LINE(3);
     x1 = linspace(-10, 10, 10000+1); x1(end) = [];
     y1 = - c/b - a/b * x1;
@@ -9,4 +10,5 @@ function GOAL = euclidian(q, LINE)
     goal_y = y1(loc);
     
     GOAL = [goal_x, goal_y];
+    
 end
