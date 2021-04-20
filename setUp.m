394800377ec94d39ee2cde5139ba6758c8a9f8f7
Chiran Walisundara <cwalisundara21@gmail.com>
2021-04-21 00:03:38 +1000

    progress

---
+++ setUp.m
@@ -0,0 +1,25 @@
+%% Enter IP address shown on robot
+IP = '172.19.232.172';
+
+%Initialise robot
+pb = PiBot(IP);
+
+%% Get image 
+img = pb.getImageFromCamera(); %captures an image and assigns it to the variable 'img'
+imshow(img); % show image
+
+
+%% Move Robot + Encoders
+%Forward 2sec
+pb.setMotorSpeeds(35,35); % Motor powers from 0-100
+pause(2)
+pb.getMotorTicks()% To see the current encoder count
+% outputs (MotorA counts, MotorB counts) 
+
+%Back
+pb.setMotorSpeeds(-35,-35);
+pause(2)
+pbgetMotorTicks()
+
+%Stop
+pb.setMotorSpeeds(0,0)
