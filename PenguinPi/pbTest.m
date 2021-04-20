d05f1d391e31d6b886475717dc330507b489fde3
chillaah <cwalisundara21@gmail.com>
2021-04-21 01:22:32 +1000

    progress

--- PenguinPi/pbTest.m
+++ PenguinPi/pbTest.m
@@ -2,8 +2,9 @@ close all; clear; clc
 
 pb = PiBot('192.168.50.5');
 
-pb.setVelocity(0,30)
-pause(5)
-pb.setVelocity(30,0)
-pause(5)
+pb.setVelocity(0,20);
+pause(3)
+pb.setVelocity(20,0);
+pb.setLED(2);
+pause(3)
 pb.setVelocity(0,0);
