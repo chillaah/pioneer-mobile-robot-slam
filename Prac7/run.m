a7a805240927b0c788ad40411f124e3e515e42b3
chillaah <cwalisundara21@gmail.com>
2021-04-30 22:47:22 +1000

    progress

--- Prac7/run.m
+++ Prac7/run.m
@@ -3,11 +3,17 @@ close all; clear; clc
 % m1r1 = data(1).image;
 % load map1r3.mat
 % m1r3 = data(1).image;
-load map1r3.mat
-img = data(1).image;
+load map2r1.mat
+
 % load map2r2.mat
 % m2r2 = data(1).image;
 % load map2r3.mat
 % m2r3 = data(1).image;
 
-Z = sense(img);
+Z = sense(data(51).image);
+
+% for i = length(data):-1:1
+%     img = data(i).image;
+%     Z = sense(img)
+% end
+
--- Prac7/sense.m
+++ Prac7/sense.m
@@ -1,107 +1,105 @@
 function Z = sense(I)
-% for week 7 task, detect IDs only
 % input: I is an RGB image of the robot's view
 % output: Z = 1xn where n is the number of beacons in the image.
 
-%for week 8 task, detect IDs and estimate range and bearing
-% input: I is an RGB image of the robot's view
-% output: Z = nx3 where Z(i,:) = [id range bearing] (bearing should be in degrees)
-
-    im = I;
-    
     % filtering number of beacons
-    [r, ~] = hsvRed2(im);
+    [r, ~] = hsvRed2(I);
     se = strel('disk', 6);
     med = medfilt2(r);
     heal = imopen(med, se);
     bw = bwlabel(heal);
     uni = unique(bw);
-    numBeacs = numel(uni);
-    dist = zeros(numBeacs, 3);
-    se = strel('disk', 4);
+    nums = numel(uni);
+    numBeacs = nums - 1;
     
-    % color filtering
-    [r, ~] = hsvRed2(im);
-    imR = imerode(r, se);
-    statsR = regionprops('table', imR,'Centroid');
-    centR = cat(1, statsR.Centroid);
-    for i = 1:size(centR,1)
-        dist(i,1) = hypot(centR(i,1), centR(i,2));
-    end
-    cents(1:size(centR,1),1:2) = centR;
-
-    [b, ~] = rgbBlue(im);
-    imB = imerode(b, se);
-    statsB = regionprops('table', imB,'Centroid');
-    centB = cat(1, statsB.Centroid);
-    for i = 1:size(centB,1)
-        dist(i,2) = hypot(centB(i,1), centB(i,2));
-    end
-    cents(1:size(centB,1),3:4) = centB;
-
-
-    [y, ~] = hsvYellow(im);
-    imY = imerode(y, se);
-    statsY = regionprops('table', imY,'Centroid');
-    centY = cat(1, statsY.Centroid);
-    for i = 1:size(centY,1)
-        dist(i,3) = hypot(centY(i,1), centY(i,2));
-    end
-    cents(1:size(centY,1),5:6) = centY;
-
-    array = [];
-
-    % labelling color and binary number
-    for i = 1:size(dist,1)
-        if dist(i,1) > dist(i,2)  && dist(i,1) > dist(i,3)
-            color(i,3) = "red";
-            array(i,5:6) = [0,1];
-        elseif (dist(i,1) > dist(i,2) && dist(i,1) < dist(i,3)) || (dist(i,1) < dist(i,2) && dist(i,1) > dist(i,3))
-            color(i,2) = "red";
-            array(i,3:4) = [0,1];
-        else
-            color(i,1) = "red";
-            array(i,1:2) = [0,1];
-        end
-
-        if dist(i,2) > dist(i,1)  && dist(i,2) > dist(i,3)
-            color(i,3) = "blue";
-            array(i,5:6) = [1,0];
-        elseif (dist(i,2) > dist(i,1) && dist(i,2) < dist(i,3)) || (dist(i,2) < dist(i,1) && dist(i,2) > dist(i,3))
-            color(i,2) = "blue";
-            array(i,3:4) = [1,0];
-        else
-            color(i,1) = "blue";
-            array(i,1:2) = [1,0];
-        end
-
-        if dist(i,3) > dist(i,1)  && dist(i,3) > dist(i,2)
-            color(i,3) = "yellow";
-            array(i,5:6) = [1,1];
-        elseif (dist(i,3) > dist(i,1) && dist(i,3) < dist(i,2)) || (dist(i,3) < dist(i,1) && dist(i,3) > dist(i,2))
-            color(i,2) = "yellow";
-            array(i,3:4) = [1,1];
-        else
-            color(i,1) = "yellow";
-            array(i,1:2) = [1,1];
-        end
-    end
-
-    % flipping array
-    array = fliplr(array); % [array(:,5:6) array(:,3:4) array(:,1:2)];
-    
-    if isempty(array)
+    if numBeacs == 0
+        
         Z = [];
-        return
-    end
+        
+    else
+        
+        dist = zeros(numBeacs, 3);
+        se = strel('disk', 4);
 
-    % convert binary to decimal
-    for i = 1:size(array,1)
-        Z(i) = bin2dec(num2str(array(i,:)));
+        % red filtering
+        [r, ~] = hsvRed2(I);
+        imR = imerode(r, se);
+        statsR = regionprops('table', imR,'Centroid');
+        centR = cat(1, statsR.Centroid);
+        for i = 1:size(centR,1)
+            dist(i,1) = hypot(centR(i,1), centR(i,2));
+        end
+        
+        % blue filtering
+        [b, ~] = hsvBlue(I);
+        imB = imdilate(b, se);
+        imB = imerode(imB, se);
+        statsB = regionprops('table', imB,'Centroid');
+        centB = cat(1, statsB.Centroid);
+        for i = 1:size(centB,1)
+            dist(i,2) = hypot(centB(i,1), centB(i,2));
+        end
+
+        % yellow filtering
+        [y, ~] = hsvYellow(I);
+        imY = imerode(y, se);
+        statsY = regionprops('table', imY,'Centroid');
+        centY = cat(1, statsY.Centroid);
+        for i = 1:size(centY,1)
+            dist(i,3) = hypot(centY(i,1), centY(i,2));
+        end
+
+        % labelling color and assigning binary number
+        array = zeros(numBeacs, 6);
+        color = strings(numBeacs, 3);
+        
+        for i = 1:size(dist,1)
+            if dist(i,1) > dist(i,2) && dist(i,1) > dist(i,3)
+                color(i,3) = "red";
+                array(i,5:6) = [0,1];
+            elseif (dist(i,1) > dist(i,2) && dist(i,1) < dist(i,3)) || (dist(i,1) < dist(i,2) && dist(i,1) > dist(i,3))
+                color(i,2) = "red";
+                array(i,3:4) = [0,1];
+            else
+                color(i,1) = "red";
+                array(i,1:2) = [0,1];
+            end
+
+            if dist(i,2) > dist(i,1) && dist(i,2) > dist(i,3)
+                color(i,3) = "blue";
+                array(i,5:6) = [1,0];
+            elseif (dist(i,2) > dist(i,1) && dist(i,2) < dist(i,3)) || (dist(i,2) < dist(i,1) && dist(i,2) > dist(i,3))
+                color(i,2) = "blue";
+                array(i,3:4) = [1,0];
+            else
+                color(i,1) = "blue";
+                array(i,1:2) = [1,0];
+            end
+
+            if dist(i,3) > dist(i,1) && dist(i,3) > dist(i,2)
+                color(i,3) = "yellow";
+                array(i,5:6) = [1,1];
+            elseif (dist(i,3) > dist(i,1) && dist(i,3) < dist(i,2)) || (dist(i,3) < dist(i,1) && dist(i,3) > dist(i,2))
+                color(i,2) = "yellow";
+                array(i,3:4) = [1,1];
+            else
+                color(i,1) = "yellow";
+                array(i,1:2) = [1,1];
+            end
+        end
+
+        % flipping array
+        array1 = array(:,5:6);
+        array2 = array(:,3:4);
+        array3 = array(:,1:2);
+        array = [array1, array2, array3];
+
+        % convert binary to decimal
+        Z =  zeros(1, numBeacs);
+        for i = 1:size(array,1)
+            Z(i) = bin2dec(num2str(array(i,:)));
+        end
     end
     
 end
-
-
 % write all your helper functions below and call them inside the function sense
-
