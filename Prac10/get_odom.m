ed227d2e885cca86cf2b74c798972ac0bd564869
chillaah <cwalisundara21@gmail.com>
2021-05-09 06:50:52 +1000

    progress

--- Prac10/get_odom.m
+++ Prac10/get_odom.m
@@ -1,7 +1,4 @@
-function [d , dth] = get_odom(new_ticks, old_ticks)
-%inputs: new_ticks , old_ticks both are 1x2 vectors [left_ticks right_ticks]
-%outputs: d  distance traveled in meters
-%        dt angle rotated in radians 
+function [d, dth] = get_odom(new_ticks, old_ticks)
 
     newLeft = new_ticks(1);
     newRight = new_ticks(2);
@@ -11,13 +8,16 @@ function [d , dth] = get_odom(new_ticks, old_ticks)
 
     leftdiff = newLeft - oldLeft;
     rightdiff = newRight - oldRight;
-
+    
+    % N - number of ticks per rotation = 370
+    % D - robot wheel diameter [m] = 0.065
+    % L - robot wheels axis [m] = 0.15 = 2 * R
     left = (2 * pi * 0.065/2 * leftdiff) / 370;
 
     right = (2 * pi * 0.065/2 * rightdiff) / 370;
 
-    d = (left + right)  / 2;
+    d = (left + right) / 2;
 
-    dth = (right - left) / 0.15; % assuming L is 2r
+    dth = (right - left) / 0.15;
 
 end
--- Prac10/move.m
+++ Prac10/move.m
@@ -1,8 +1,4 @@
 function next_pose = move(current_pose, d, dth)
-%inputs: current_pose is a 3x1 vector [x; y; theta] of the robot (theta in radians)
-%        d  distance traveled in meters
-%        dt angle rotated in radians 
-%outputs: next_pose is a 3x1 vector [x; y; theta] of the robot (theta in radians)
 
     currx = current_pose(1);
     curry = current_pose(2);
@@ -11,7 +7,7 @@ function next_pose = move(current_pose, d, dth)
     movedx = d * cos(currtheta);
     movedy = d * sin(currtheta);
     movedtheta = dth;
-
+    
     nextx = currx + movedx;
     nexty = curry + movedy;
     nexttheta = currtheta + movedtheta;
